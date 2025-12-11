50cdded71f1b1c5e71b891a99b42b5cc33863189
Oded Regev <odedre@gmail.com>
2025-12-11 11:34:21 +0200

    first XCFramework release

---
+++ STNPlayerSDK.xcframework/ios-arm64/STNPlayerSDK.framework/Headers/STNPlayerSDK.h
@@ -0,0 +1,16 @@
+//
+//  STNPlayerSDK.h
+//  STNPlayerSDK
+//
+//  Umbrella header for STNPlayerSDK framework
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for STNPlayerSDK.
+FOUNDATION_EXPORT double STNPlayerSDKVersionNumber;
+
+//! Project version string for STNPlayerSDK.
+FOUNDATION_EXPORT const unsigned char STNPlayerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MinuteMediaSDK/PublicHeader.h>
